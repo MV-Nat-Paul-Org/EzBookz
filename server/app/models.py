681841b1eff565aae6d4201429f8d73688fd0887
pmolina90 <P.a.molina90@gmail.com>
2024-05-17 11:32:33 -0400

    Added
    defined models

--- server/app/models.py
+++ server/app/models.py
@@ -1,1 +1,27 @@
 # Create Appointment Model here
+# Create Appointment Model here
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+
+class Appointment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    available = db.Column(db.Boolean, default=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('appointments', lazy=True))
+    admin_id = db.Column(db.Integer, db.ForeignKey('admin.id'))
+    admin = db.relationship('Admin', backref=db.backref('appointments', lazy=True))
+
